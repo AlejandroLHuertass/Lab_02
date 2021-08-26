99c389ba3746a5da9ec95855dde6bc7730e8b866
CarlosSorza <78982514+CarlosSorza@users.noreply.github.com>
2021-08-25 22:14:35 -0500

    Patterns

---
+++ Documents/2021-2/CVDS/Lab2/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/RegularShapeType.java
@@ -0,0 +1,5 @@
+package main.java.edu.eci.cvds.patterns.shapes;
+
+public enum RegularShapeType {
+    Triangle, Quadrilateral, Pentagon, Hexagon
+}
---
+++ Documents/2021-2/CVDS/Lab2/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Pentagon.java
@@ -0,0 +1,9 @@
+package main.java.edu.eci.cvds.patterns.shapes.concrete;
+
+import edu.eci.cvds.patterns.shapes.Shape;
+
+public class Pentagon implements Shape {
+    public int getNumberOfEdges() {
+        return 5;
+    }
+}
